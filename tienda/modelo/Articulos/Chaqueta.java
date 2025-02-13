7c814fa9a759a7ae09605a957bb793c4a5693c8f
Miguel Grau <miggrafer@alu.edu.gva.es>
2025-02-13 13:26:41 +0100

    fin de dia 2

---
+++ tienda/modelo/Articulos/Chaqueta.java
@@ -0,0 +1,51 @@
+package iescamp.tienda.modelo.Articulos;
+
+import java.util.Objects;
+
+public class Chaqueta extends Ropa{
+    private Boolean impermeable;
+    //GETTER Y SETTER
+    public Boolean getImpermeable() {
+        return impermeable;
+    }
+
+    public void setImpermeable(Boolean impermeable) {
+        this.impermeable = impermeable;
+    }
+    //CONSTRUCTOR
+
+    public Chaqueta(Material material, int cod_art, boolean activo, String color, String imagen, String nombre, double precio, String marca, String descripcion, int talla, String color1, String tipoCierre, Boolean impermeable) {
+        super(material, cod_art, activo, color, imagen, nombre, precio, marca, descripcion, talla, color1, tipoCierre);
+        this.impermeable = impermeable;
+    }
+
+
+    //mostrarDetalles
+
+    @Override
+    public String toString() {
+        return "Chaqueta{" +
+                "impermeable=" + impermeable +
+                '}';
+    }
+
+    public String mostrarDetalles() {
+        return super.toString() + "Chaqueta{" +
+                "impermeable=" + impermeable +
+                '}';
+    }
+    //EQUALS Y HASHCODE
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Chaqueta chaqueta = (Chaqueta) o;
+        return Objects.equals(impermeable, chaqueta.impermeable);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), impermeable);
+    }
+}
---
+++ tienda/modelo/Articulos/Ropa.java
@@ -0,0 +1,66 @@
+package iescamp.tienda.modelo.Articulos;
+
+import java.util.Objects;
+
+public class Ropa extends Articulo{
+    private int talla;
+    private String color;
+    private String tipoCierre;
+
+    //Getter y Setter
+    public int getTalla() {
+        return talla;
+    }
+
+    public void setTalla(int talla) {
+        this.talla = talla;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipoCierre() {
+        return tipoCierre;
+    }
+
+    public void setTipoCierre(String tipoCierre) {
+        this.tipoCierre = tipoCierre;
+    }
+    //Constructor
+
+
+    public Ropa(Material material, int cod_art, boolean activo, String color, String imagen, String nombre, double precio, String marca, String descripcion, int talla, String color1, String tipoCierre) {
+        super(material, cod_art, activo, color, imagen, nombre, precio, marca, descripcion);
+        this.talla = talla;
+        this.color = color1;
+        this.tipoCierre = tipoCierre;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return super.toString() + "Ropa{" +
+                "talla=" + talla +
+                ", color='" + color + '\'' +
+                ", tipoCierre='" + tipoCierre + '\'' +
+                '}';
+    }
+    //Equals y HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ropa ropa = (Ropa) o;
+        return talla == ropa.talla && Objects.equals(color, ropa.color) && Objects.equals(tipoCierre, ropa.tipoCierre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(talla, color, tipoCierre);
+    }
+}
