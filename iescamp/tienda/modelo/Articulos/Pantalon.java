353783a683c4651d40931f50988bd75fd59d21dc
Alba Rumeu <albrummon@alu.edu.gva.es>
2025-02-26 12:50:41 +0100

    Add files via upload

---
+++ iescamp/tienda/modelo/Articulos/Pantalon.java
@@ -0,0 +1,49 @@
+package iescamp.tienda.modelo.Articulos;
+
+public class Pantalon extends Ropa{
+    private Boolean tieneBolsillos;
+    private String tipoPantalon;
+
+    //GETTER Y SETTERS
+
+    public Boolean getTieneBolsillos() {
+        return tieneBolsillos;
+    }
+
+    public void setTieneBolsillos(Boolean tieneBolsillos) {
+        this.tieneBolsillos = tieneBolsillos;
+    }
+
+    public String getTipoPantalon() {
+        return tipoPantalon;
+    }
+
+    public void setTipoPantalon(String tipoPantalon) {
+        this.tipoPantalon = tipoPantalon;
+    }
+    //CONSTRUCTOR
+
+    public Pantalon(Material material, int cod_art, boolean activo, String color, String imagen, String nombre, double precio, String marca, String descripcion, int talla, String color1, String tipoCierre, Boolean tieneBolsillos, String tipoPantalon) {
+        super(material, cod_art, activo, color, imagen, nombre, precio, marca, descripcion, talla, color1, tipoCierre);
+        this.tieneBolsillos = tieneBolsillos;
+        this.tipoPantalon = tipoPantalon;
+    }
+
+
+    //MOSTRARDETALLES
+
+    @Override
+    public String toString() {
+        return "Pantalon{" +
+                "tieneBolsillos=" + tieneBolsillos +
+                ", tipoPantalon='" + tipoPantalon + '\'' +
+                '}';
+    }
+
+    public String MostrarDetalles() {
+        return super.toString() + "Pantalon{" +
+                "tieneBolsillos=" + tieneBolsillos +
+                ", tipoPantalon='" + tipoPantalon + '\'' +
+                '}';
+    }
+}
---
+++ iescamp/tienda/test/Test.java
@@ -0,0 +1,12 @@
+package iescamp.tienda.test;
+
+import com.iescamp.tienda.modelo.Cliente;
+import com.iescamp.tienda.modelo.Empleado;
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
